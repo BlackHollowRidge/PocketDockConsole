502a255e6e145d087eacd7be04d8b9f11ef6bc6f
Crush3r <crushedpvppe@gmail.com>
2018-07-27 23:52:17 -0400

    Update Main.php

--- src/PocketDockConsole/Main.php
+++ src/PocketDockConsole/Main.php
@@ -4,27 +4,31 @@ namespace PocketDockConsole;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
 use pocketmine\command\Command;
-use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\CommandSender;
-use pocketmine\scheduler\PluginTask;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\ThreadManager;
 
 class Main extends PluginBase implements Listener {
 
-	public function onLoad() {
-		$this->getLogger()->info(TextFormat::WHITE . "Loaded");
-	}
+    public function onLoad() {
+        $this->getLogger()->info(TextFormat::WHITE . "Loaded");
+    }
 
-	public function onEnable() {
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->saveDefaultConfig();
-		$this->reloadConfig();
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->reloadConfig();
         $this->getLogger()->info(TextFormat::DARK_GREEN . "Enabled");
         $this->setPassword();
-        $this->thread = new SocksServer("0.0.0.0", $this->getConfig()->get("port"), $this->getServer()->getLogger(), $this->getServer()->getLoader(), $this->getConfig()->get("password"), stream_get_contents($this->getResource("PluginIndex.html")), $this->getConfig()->get("backlog"));
+        $this->legacy = true;
+        if (method_exists($this->getServer(), "getNetwork")) {
+            $this->legacy = false;
+        }
+        $this->thread = new PDCServer("0.0.0.0", $this->getConfig()->get("port"), $this->getServer()->getLogger(), $this->getServer()->getLoader(), $this->getConfig()->get("password"), stream_get_contents($pluginIndex = $this->getResource("PluginIndex.html")), $this->getConfig()->get("backlog"), $this->legacy);
+        @fclose($pluginIndex);
         $this->rc = new RunCommand($this);
         $this->getServer()->getScheduler()->scheduleRepeatingTask($this->rc, 1);
         $this->lastBufferLine = "";
@@ -46,26 +50,26 @@ class Main extends PluginBase implements Listener {
         switch ($command->getName()) {
             case "consoleclients":
                 if (!$sender->hasPermission("pocketdockconsole.command.consoleclients")) {
-                    $sender->sendMessage(TextFormat::RED . "[PocketDockConsole] Get some permissions...");
+                    $sender->sendMessage(TextFormat::RED . "[PDC] Get some permissions...");
                     return true;
                 }
                 $authedclients = explode(";", $this->thread->connectedips);
                 if (count($authedclients) < 2) {
-                    $sender->sendMessage("[PocketDockConsole] There are no connected clients");
+                    $sender->sendMessage("[PDC] There are no connected clients");
                     return true;
                 }
-                $sender->sendMessage("[PocketDockConsole] Connected client(s) are: " . implode("; ", $authedclients));
+                $sender->sendMessage("[PDC] Connected client(s) are: " . implode("; ", $authedclients));
                 return true;
             case "killclient":
                 if (!$sender->hasPermission("pocketdockconsole.command.killclient")) {
-                    $sender->sendMessage(TextFormat::RED . "[PocketDockConsole] Get some permissions...");
+                    $sender->sendMessage(TextFormat::RED . "[PDC] Get some permissions...");
                     return true;
                 }
                 if (!isset($args[0])) {
                     $sender->sendMessage($command->getUsage());
                     return true;
                 }
-                $sender->sendMessage("[PocketDockConsole] Killing client: " . $args[0]);
+                $sender->sendMessage("[PDC] Killing client: " . $args[0]);
                 $this->thread->clienttokill = $args[0];
                 return true;
             default:
@@ -98,15 +102,16 @@ class Main extends PluginBase implements Listener {
     }
 
     public function sendFiles() {
-        if($this->getConfig()->get("viewfiles")) {
+        if ($this->getConfig()->get("viewfiles")) {
             $this->thread->jsonStream.= json_encode(array("type" => "files", "files" => $this->getFiles(realpath($this->getServer()->getDataPath())))) . "\n";
         }
         return false;
     }
 
     public function onDisable() {
-        $this->getServer()->getLogger()->removeAttachment($this->attachment);
         $this->getLogger()->info(TextFormat::DARK_RED . "Disabled");
+        //$this->getServer()->getLogger()->removeAttachment($this->attachment);
+        ThreadManager::getInstance()->remove($this->thread);
         $this->thread->stop();
     }
 
